5afa106691720bb3e042660cf71870ebba24b062
daniel <102236708+daniel-lou18@users.noreply.github.com>
2023-04-28 05:34:48 +0000

    Update App.js and index.css

--- src/App.js
+++ src/App.js
@@ -1,20 +1,52 @@
-import { Route, Switch } from 'react-router-dom';
-import Home from './components/Home';
-import Stocks from './components/Stocks';
-import Movies from './components/Movies';
+import { Route, Switch, Link, NavLink } from "react-router-dom";
+import Home from "./components/Home";
+import Stocks from "./components/Stocks";
+import Movies from "./components/Movies";
 
 function App() {
   return (
-    <div className='main'>
+    <div className="main">
       <h1>App Component</h1>
+      <nav className="comp nav">
+        <ul>
+          <li>
+            <NavLink
+              activeStyle={{ fontWeight: "bold" }}
+              activeClassName="purple"
+              exact
+              to="/"
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              activeStyle={{ fontWeight: "bold" }}
+              activeClassName="purple"
+              to="/stocks"
+            >
+              Stocks
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              activeStyle={{ fontWeight: "bold" }}
+              activeClassName="purple"
+              to="/movies"
+            >
+              Movies
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
       <Switch>
-        <Route exact path='/'>
+        <Route exact path="/">
           <Home />
         </Route>
-        <Route path='/stocks'>
+        <Route path="/stocks">
           <Stocks />
         </Route>
-        <Route path='/movies'>
+        <Route path="/movies">
           <Movies />
         </Route>
         <Route>
